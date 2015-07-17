cf967c40f26e3a92ed5656381cb9680706c66516
caseylouisee <caseylouisee@me.com>
2015-07-17 11:49:53 +0100

    update

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -9,27 +9,27 @@
 	<h1>Jobs</h1>
 	<hr />
 	<div class='col-md-12'>
-	@foreach($job as $jobs)
-		<div class='col-md-4'>
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<strong>{{$jobs->title}}</strong>
-			</div> <!-- class="panel-heading" -->
-			<div class="panel-body">
-				@if(strlen($jobs->description)>100)
-					<p> 
-						<strong>Description:</strong>
-						{{substr($jobs->description,0,100)}} ...
-						<a href="/jobs/{{$jobs->id}}">Read More</a> 
-					</p>
-				@else
-					<p> Description: {{$jobs->description}} </p>
-				@endif
-			</div> <!-- class="panel-body" -->
-		</div> <!-- class="panel panel-default" -->
-		</div>
-	@endforeach
-	</div>
+		@foreach($job as $jobs)
+			<div class='col-md-4'>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<strong>{{$jobs->title}}</strong>
+					</div> <!-- class="panel-heading" -->
+					<div class="panel-body">
+						@if(strlen($jobs->description)>100)
+							<p> 
+								<strong>Description:</strong>
+								{{substr($jobs->description,0,100)}} ...
+								<a href="/jobs/{{$jobs->id}}">Read More</a> 
+							</p>
+						@else
+							<p> Description: {{$jobs->description}} </p>
+						@endif
+					</div> <!-- class="panel-body" -->
+				</div> <!-- class="panel panel-default" -->
+			</div> <!-- class="col-md-4" -->
+		@endforeach
+	</div> <!-- class="col-md-12" -->
 
 	{!! $job->render() !!}
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,36 +19,45 @@
 	
 	<h1>Profiles</h1>
 	<hr />
-	</br>
 	@foreach($user->get() as $users)
-	<div class="row">
-	<div class="well col-md-12">
-	<div class="col-md-2">
-		@if(File::exists('./images/profiles/' . $users->id . '.jpg')) 
-			<img src="/images/profiles/{{$users->id}}.jpg" alt="" class="img-circle img-center">
-		@else 
-			<img src="{{$users->image}}" alt="" class="img-circle img-center">
-		@endif
-	</div>
-	<div class="col-md-10">
-		<p><a href="/users/{{$users->id}}">{{$users->name}}</a></p>
-		@for ($i = 0; $i != $users->ratings()->avg('rating'); $i++)
-			<span class="pull-right fa fa-star"></span>
-		@endfor
-		<p>{{$users->description}}</p>
-		@unless($users->tags->isEmpty())
-			<p> 
-			Skills:
-			@foreach($users->tags as $tag)
-				<span class="label label-info">{{$tag->name}}</span>
-			@endforeach
-			</p>
-		@endunless
-	</div>	
-	</div>
-	</div> <!-- class="row" -->
 	
-	</br>
+		<!-- style 1 -->
+		<div class="row">
+			<div class="well col-md-12">
+				<div class="col-md-2">
+					@if(File::exists('./images/profiles/' . $users->id . '.jpg')) 
+						<img src="/images/profiles/{{$users->id}}.jpg" alt="" class="img-circle img-center">
+					@else 
+						<img src="{{$users->image}}" alt="" class="img-circle img-center">
+					@endif
+				</div> <!-- class="col-md-2" -->
+				<div class="col-md-10">
+					<p><strong><a href="/users/{{$users->id}}">{{$users->name}}</a></strong>
+					@for ($i = 0; $i != $users->ratings()->avg('rating'); $i++)
+						<span class="pull-right fa fa-star"></span>
+					@endfor
+					</p>
+					@if(strlen($users->description)>150)
+						<p> 
+							{{substr($users->description,0,150)}} ...
+						</p>
+					@else
+						<p> {{$users->description}} </p>
+					@endif
+					
+					@unless($users->tags->isEmpty())
+						<p> 
+							Skills:
+							@foreach($users->tags as $tag)
+								<span class="label label-info">{{$tag->name}}</span>
+							@endforeach
+						</p>
+					@endunless
+				</div> <!-- class="col-md-10" -->
+			</div> <!-- class="well col-md-12" -->
+		</div> <!-- class="row" -->
+		
+		<!-- style 2 -->
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<a href="/users/{{$users->id}}">{{$users->name}}</a>
@@ -68,6 +77,7 @@
 				@endunless
 			</div> <!-- class="panel-body" -->
 		</div> <!-- class="panel panel-default" -->
+		
 	@endforeach
 
 @endsection
